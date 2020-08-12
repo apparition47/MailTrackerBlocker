e641919b0ec969590990300bf03f82fbe608746c
Aaron Lee <aaron@onefatgiraffe.com>
2020-08-12 13:13:30 +0900

    Fix Cask updating

--- mailtrackerblocker.rb
+++ mailtrackerblocker.rb
@@ -3,6 +3,7 @@ cask "mailtrackerblocker" do
   sha256 "1b62f35ef383e829448c5e12b25a4e8996aafcae6bdfe012f637cf0802889b44"
   
   url "https://github.com/apparition47/MailTrackerBlocker/releases/download/#{version}/MailTrackerBlocker.pkg"
+  appcast "https://github.com/apparition47/MailTrackerBlocker/releases.atom"
   name "MailTrackerBlocker"
   homepage "https://apparition47.github.io/MailTrackerBlocker/"
   depends_on macos: ">= :high_sierra"
