d37d759d455a981217d013ca6401e22719d72ffe
Daniel Dickison <git@danieldickison.com>
2021-02-27 12:49:38 -0800

    Add unit test and make it so generic spy pixel regex is always checked last, to prevent being dependent on dictionary iteration order.

--- Source/MTBBlockedMessage.m
+++ Source/MTBBlockedMessage.m
@@ -10,43 +10,40 @@
 
 @interface MTBBlockedMessage ()
 @property (nonatomic, copy) NSString *sanitizedHtml;
-@property (nonatomic, retain) NSMutableDictionary *results;
+@property (nonatomic, retain) NSMutableSet *trackers;
+@property BOOL matchedGeneric;
 @property (nonatomic, weak) id <MTBBlockedMessageDelegate> delegate;
 @end
 
 @implementation MTBBlockedMessage
 
 NSString *kGenericSpyPixel = @"_Generic Spy Pixel";
+NSString *kGenericSpyPixelRegex = @"<img[^>]+(1px|\"1\"|'1')+[^>]*>";
 
-@synthesize results, delegate;
+@synthesize trackers, delegate;
 
 - (id)initWithHtml:(NSString*)html {
     self = [self init];
     if (!self) {
         return nil;
     }
-    results = [[NSMutableDictionary alloc] init];
+    trackers = [[NSMutableSet alloc] init];
     _sanitizedHtml = [self sanitizedHtmlFromHtml: html];
     return self;
 }
 
 - (NSString *)detectedTracker {
-    for (NSString *key in results) {
-        if ([key isEqualToString:kGenericSpyPixel]) {
-            continue;
-        }
-        return key;
-    }
-    return nil;
+    return [trackers anyObject];
 }
 
 - (enum BLOCKING_RESULT_CERTAINTY)certainty {
-    if ([results count] == 0) {
-        return BLOCKING_RESULT_CERTAINTY_LOW_NO_MATCHES;
-    } else if ([results count] == 1 && [results objectForKey:kGenericSpyPixel]) {
+    if ([trackers count] > 0) {
+        return BLOCKING_RESULT_CERTAINTY_CONFIDENT_HARD_MATCH;
+    } else if (_matchedGeneric) {
         return BLOCKING_RESULT_CERTAINTY_MODERATE_HEURISTIC;
+    } else {
+        return BLOCKING_RESULT_CERTAINTY_LOW_NO_MATCHES;
     }
-    return BLOCKING_RESULT_CERTAINTY_CONFIDENT_HARD_MATCH;
 }
 
 - (NSString*)sanitizedHtml {
@@ -62,12 +59,21 @@ NSString *kGenericSpyPixel = @"_Generic Spy Pixel";
             NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:regexStr options:NSRegularExpressionCaseInsensitive error:NULL];
             NSRange range = NSMakeRange(0, result.length);
             NSString *replaced = [regex stringByReplacingMatchesInString:result options:0 range:range withTemplate:@""];
-            if (replaced != result) {
-                results[trackingSourceKey] = result;
+            if (![replaced isEqualToString:result]) {
+                [trackers addObject:trackingSourceKey];
                 result = replaced;
             }
         }
     }
+    
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:kGenericSpyPixelRegex options:NSRegularExpressionCaseInsensitive error:NULL];
+    NSRange range = NSMakeRange(0, result.length);
+    NSString *replaced = [regex stringByReplacingMatchesInString:result options:0 range:range withTemplate:@""];
+    if (![replaced isEqualToString:result]) {
+        _matchedGeneric = YES;
+        result = replaced;
+    }
+
     return result;
 }
 
@@ -327,8 +333,6 @@ NSString *kGenericSpyPixel = @"_Generic Spy Pixel";
         @"Yesware": @[@"yesware.com/trk", @"yesware.com/t/", @"t.yesware.com"],
         @"Zapier": @[@"opens.zapier.com/q/(.*)/(.*)/(.*)~~"],
         @"Zendesk": @[@"futuresimple.com/api/v1/sprite.png"],
-
-        kGenericSpyPixel: @[@"<img[^>]+(1px|\"1\"|'1')+[^>]*>"]
     };
 }
 @end
---
+++ Unit Tests/MTBBlockedMessageTest.m
@@ -0,0 +1,70 @@
+//
+//  MTBBlockedMessageTest.m
+//  Unit Tests
+//
+//  Created by Daniel Dickison on 2021-02-27.
+//
+
+#import <XCTest/XCTest.h>
+#import "MTBBlockedMessage.h"
+
+#define CLEAN_HTML @"<p>This is an email without any trackers <img src='https://example.com/foo.png' width='32' height='16' style='width: 32px; height: 16px; border: 1px solid gray;'> blah </p>"
+#define SENDGRID_HTML @"<p>This is an email with a sendgrid tracker <img src='https://sendgrid.com/trk/123ef89329817898/3248932743' width='1' height='1' style='width: 1px; height: 1px;'></p>"
+#define SENDGRID_CLEANED @"<p>This is an email with a sendgrid tracker <img src='/123ef89329817898/3248932743' width='1' height='1' style='width: 1px; height: 1px;></p>"
+#define PIXEL_HTML @"<p>This is an email with a generic tracking pixel <img src='https://example.com/foo/123ef89329817898/3248932743' width='1' height='1' style='width: 1px; height: 1px;'></p>"
+#define PIXEL_CLEANED @"<p>This is an email with a generic tracking pixel </p>"
+#define SENDGRID_HTML @"<p>This is an email with a sendgrid tracker <img src='https://sendgrid.com/trk/123ef89329817898/3248932743' width='1' height='1' style='width: 1px; height: 1px;'></p>"
+
+@interface MTBBlockedMessageTest : XCTestCase
+@end
+
+@implementation MTBBlockedMessageTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testCleanHtml {
+    NSString *cleanHTML = @"<p>This is an email without any trackers <img src='https://example.com/foo.png' width='32' height='16' style='width: 32px; height: 16px; border: 1px solid gray;'> blah </p>";
+    XCTAssertEqualObjects([[MTBBlockedMessage alloc] initWithHtml:cleanHTML].sanitizedHtml,
+                   cleanHTML);
+    XCTAssertEqual([[MTBBlockedMessage alloc] initWithHtml:cleanHTML].sanitizedHtml,
+                   cleanHTML,
+                   @"Original string should be referenced directly for clean HTML");
+}
+
+- (void)testSendgrid {
+    MTBBlockedMessage *msg = [[MTBBlockedMessage alloc] initWithHtml:@"<p>This is an email with a sendgrid tracker <img src='https://sendgrid.com/trk/123ef89329817898/3248932743' width='1' height='1' style='width: 1px; height: 1px;'></p>"];
+    XCTAssertEqualObjects(msg.sanitizedHtml,
+                          @"<p>This is an email with a sendgrid tracker </p>");
+    XCTAssertEqualObjects(msg.detectedTracker, @"Sendgrid");
+    XCTAssertEqual(msg.certainty, BLOCKING_RESULT_CERTAINTY_CONFIDENT_HARD_MATCH);
+}
+
+- (void)testGenericPixel {
+    MTBBlockedMessage *msg = [[MTBBlockedMessage alloc] initWithHtml:@"<p>This is an email with a generic pixel tracker <img src='https://example.com/foo/123ef89329817898/3248932743' width='1' height='1' style='width: 1px; height: 1px;'></p>"];
+    XCTAssertEqualObjects(msg.sanitizedHtml,
+                          @"<p>This is an email with a generic pixel tracker </p>");
+    XCTAssertEqual(msg.certainty, BLOCKING_RESULT_CERTAINTY_MODERATE_HEURISTIC);
+}
+
+- (void)testMultiplePixels {
+    MTBBlockedMessage *msg = [[MTBBlockedMessage alloc] initWithHtml:@"<p>This is an email with multiple generic pixel trackers <img src='https://example.com/foo/123ef89329817898/3248932743' width='1' height='1' style='width: 1px; height: 1px;'></p>\n<img src='https://foo.com/bar/xyzsdfji' style='width: 1px; height: 1px;'>"];
+    XCTAssertEqualObjects(msg.sanitizedHtml,
+                          @"<p>This is an email with multiple generic pixel trackers </p>\n");
+    XCTAssertEqual(msg.certainty, BLOCKING_RESULT_CERTAINTY_MODERATE_HEURISTIC);
+}
+
+- (void)testMultipleTrackers {
+    MTBBlockedMessage *msg = [[MTBBlockedMessage alloc] initWithHtml:@"<p>This is an email with sendgrid and adobe trackers <img src='https://sendgrid.net/trk/123ef89329817898/3248932743' width='1' height='1' style='width: 1px; height: 1px;'></p>\n<img src='https://demex.com/123456' style='width: 1px; height: 1px;'>"];
+    XCTAssertEqualObjects(msg.sanitizedHtml,
+                          @"<p>This is an email with sendgrid and adobe trackers </p>\n");
+    XCTAssertEqualObjects(msg.detectedTracker, @"Sendgrid"); // Or Adobe? Depends on dictionary key order
+    XCTAssertEqual(msg.certainty, BLOCKING_RESULT_CERTAINTY_CONFIDENT_HARD_MATCH);
+}
+
+@end
